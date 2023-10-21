03f10b8e17e55d98288030c98541c54b1d446093
freire <freireg1503@gmail.com>
2023-10-20 22:28:25 -0300

    Workspace setup

---
+++ spi/app/inc/source.h
@@ -0,0 +1,22 @@
+/**
+ * @file test.h
+ * @author Guilherme Freire (guilherme.freire@fieb.org.br)
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef C892DFAF_E22D_476B_9837_82F3853B6E36
+#define C892DFAF_E22D_476B_9837_82F3853B6E36
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#include <wiringx.h>
+
+
+#endif /* C892DFAF_E22D_476B_9837_82F3853B6E36 */
---
+++ spi/app/src/source.c
@@ -0,0 +1,19 @@
+/**
+ * @file main.c
+ * @author Guilherme Freire (guilherme.freire@fieb.org.br)
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "source.h"
+
+
+int main()
+{
+
+  return 0;
+}
